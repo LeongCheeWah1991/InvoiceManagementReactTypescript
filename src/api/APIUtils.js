7f3e4754e72a8e03f0721b5be40066d2383bf836
LeongCheeWah1991 <cheewah_1991@hotmail.com>
2023-02-21 16:39:02 +0800

    added uploadInvoice API

--- src/api/APIUtils.js
+++ src/api/APIUtils.js
@@ -24,3 +24,29 @@ export const getRequest = (path, next, param) => {
 			});
 		});
 };
+
+export const uploadRequest = (path, next, body) => {
+	console.log('body', body)
+
+	const headers = {
+		'Content-Type': 'application/json',
+	};
+
+	let formData = new FormData();
+	formData.append('file', body.file);
+
+	axios
+		.post(host + path, formData, headers)
+		.then((response) => {
+			next({
+				error: false,
+				data: 'success',
+			});
+		})
+		.catch((err) => {
+			next({
+				error: true,
+			});
+		});
+};
+
--- src/api/InvoiceAPI.js
+++ src/api/InvoiceAPI.js
@@ -1,6 +1,11 @@
 import {
-	getRequest} from './APIUtils';
+	getRequest,
+	uploadRequest} from './APIUtils';
 
 export const getInvoices = (next, param) => {
 	getRequest('invoice/', next, param);
+};
+
+export const uploadInvoices = (next, param) => {
+	uploadRequest('invoice/upload', next, param);
 };
